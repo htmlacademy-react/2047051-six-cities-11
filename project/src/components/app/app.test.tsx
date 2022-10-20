8dcd7a30c702444eee7e5a7b53ce05e9b44733fc
andreev-oe <andreev.oe@yandex.ru>
2022-10-20 23:59:51 +0300

    feat: add main page component
    - add main page component
    - add card component
    - pass props to main component

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,8 +1,9 @@
 import {render, screen} from '@testing-library/react';
 import App from './app';
+import {Data} from '../../constants';
 
 test('Renders app-component', () => {
-  render(<App />);
+  render(<App offersAmount={Data.OFFERS_AMOUNT}/>);
   const textElement = screen.getByText(/Hello, world!/i);
   expect(textElement).toBeInTheDocument();
 });
