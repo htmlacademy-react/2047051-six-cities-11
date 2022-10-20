f353ed6f3ba14d8cc3e3c760229c3eb6982c27a4
andreev-oe <andreev.oe@yandex.ru>
2022-10-21 02:26:34 +0300

    fix: delete app.test.tsx

--- project/src/components/app/app.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from './app';
-import {Data} from '../../constants';
-import {render, screen} from '@testing-library/react';
-
-test('Renders app-component', () => {
-  render(<App offersAmount={Data.OFFERS_AMOUNT}/>);
-  const textElement = screen.getByText(/Hello, world!/i);
-  expect(textElement).toBeInTheDocument();
-});
